154d463d5bcfea661faed8fc5afcac5005d164a1
Manishankar810 <53077560+Manishankar810@users.noreply.github.com>
2019-07-19 15:47:45 +0530

    Create conso.py

---
+++ conso.py
@@ -0,0 +1,9 @@
+#manishankar810@gmail.com
+a=['a','e','i','o','u']
+b=str(input())
+if b in a and b is str:
+    print('vowel')
+elif b not in a and b is str:
+    print('consonant')
+else:
+    print('invalid')
